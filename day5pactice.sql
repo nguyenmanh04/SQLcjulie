de9457f0a66046cbe5d320e62c028d137aaf046e
nguyenmanh04 <manhmit151004@gmail.com>
2025-02-06 12:44:51 +0700

    Update day5pactice.sql

--- day5pactice.sql
+++ day5pactice.sql
@@ -26,3 +26,41 @@ from posts
 where post_date>='2021-01-01' and post_date <='2022-01-01'
 group by user_id
 having count(post_id)>2
+
+--baitap7 
+SELECT card_name,
+(max(issued_amount)-min(issued_amount)) as difference
+from monthly_cards_issued
+group by card_name
+order by card_name desc
+
+--baitap8
+SELECT manufacturer ,
+count(drug)as drug_count,
+abs(sum(cogs-total_sales)) as total_loss
+from pharmacy_sales
+where cogs > total_sales
+group by manufacturer
+
+--baitap9
+select * from cinema
+where id%2=1 and description not like '%boring%'
+order by rating desc
+
+--baitap10
+select teacher_id ,
+count(distinct(subject_id)) as cnt
+from teacher 
+group by teacher_id
+
+--baitap11
+select user_id,
+count(follower_id) as followers_count 
+ from followers
+ group by user_id
+
+--baitap12
+select class from courses
+group by class
+having count(student) >=5
+
